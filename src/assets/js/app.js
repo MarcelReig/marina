414e0a74aa8a5ba9e4832b1963493a34636f25e2
Marcel Reig <marcelreig@gmail.com>
2017-05-10 21:01:52 +0200

    cambio de light-box

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,9 +1,7 @@
 $(document).foundation();
 
-
 /* Sticky footer */
-
- $(window).bind("load", function () {
+$(window).bind("load", function() {
     var footer = $("footer");
     var pos = footer.position();
     var height = $(window).height();
@@ -16,19 +14,45 @@ $(document).foundation();
     }
 });
 
-/* absoluteBox */
+/* masonry */
+$(window).load(function() {
+    $('.grid').masonry({
+        // set itemSelector so .grid-sizer is not used in layout
+        itemSelector: '.grid-item',
+        // use element for option
+        columnWidth: '.grid-sizer',
+        gutter: 10,
+        isFitWidth: true
+    });
+});
 
-var $grid = $( ".masonry" ).imagesLoaded( function() {
-  $grid.masonry({
-    itemSelector: ".item-selector",
-    fitWidth: true
-  });
-} );
+/* light box magnificPopup */
+$(document).ready(function() {
+    $('.image-link').magnificPopup({
+        gallery: {
+            enabled: true, // set to true to enable gallery
 
-$( ".gallery" ).absoluteBox( {
-  icons: {
-    quitter: "<i class='material-icons'>&#xE5CD;</i>",
-    prev: "<i class='material-icons'>&#xE5CB;</i>",
-    next: "<i class='material-icons'>&#xE5CC;</i>"
-  }
-} );
+            preload: [0, 2], // read about this option in next Lazy-loading section
+
+            navigateByImgClick: true,
+
+            arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
+
+            tPrev: 'Previous (Left arrow key)', // title for left button
+            tNext: 'Next (Right arrow key)', // title for right button
+            tCounter: '<span class="mfp-counter">%curr% of %total%</span>' // markup of counter
+        },
+        type: 'image'
+    });
+});
+
+$(document).ready(function() {
+    $('.parent-container').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        // other options
+        gallery: {
+            enabled: true
+        }
+    });
+});
